3a6f34088b1747d2bba02b44c42206606bf3fff7
morpheusthewhite <zffromGerace@hotmail.it>
2018-05-10 20:24:33 +0200

    Removed unnecessary buttons and toast

--- studia/app/src/main/java/com/example/stefano/fabio/MainActivity.java
+++ studia/app/src/main/java/com/example/stefano/fabio/MainActivity.java
@@ -10,10 +10,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
-import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         switcher = (ImageSwitcher) findViewById(R.id.switcher);
 
         View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION  |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION  |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         FullScreen fullScreen = new FullScreen(decorView);
         Thread onFull = new Thread(fullScreen);
@@ -48,10 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         switcher.setImageResource(R.drawable.gigiproietti);
 
-        Animation animation = AnimationUtils.loadAnimation(this,android.R.anim.slide_in_left);
-
-        //Button b2 = (Button) findViewById(R.id.button2);
-        //Button b1 = (Button) findViewById(R.id.button);
+        //Animation animation = AnimationUtils.loadAnimation(this,android.R.anim.slide_in_left);
 
     }
 
@@ -64,15 +59,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void LeftTap()
     {
-        Log.e("Tap", "Left");
         switcher.setImageResource(R.drawable.gigiproietti);
     }
 
     private void RightTap()
     {
-        Log.e("Tap", "Right");
-        Toast.makeText(getApplicationContext(), "Next Image",
-                Toast.LENGTH_SHORT).show();
         switcher.setImageResource(R.drawable.gigidag);
     }
 
@@ -83,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(MotionEvent... motionEvents) {
             MotionEvent event = motionEvents[0];
             int x = (int)event.getX();
-            int y = (int)event.getY();
 
             if(DisplayWidth == 0)
             {
